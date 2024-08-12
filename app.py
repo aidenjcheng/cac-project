62bbaf4232086b53c90d3c04c4cd603f1c62c673
ddddddd1888 <aidenjcheng12@gmail.com>
2024-08-11 22:23:10 -0400

    chore: Update app.py to use environment variable for port number

--- app.py
+++ app.py
@@ -252,12 +252,9 @@ def logout():
   
 
 
-
 if __name__ == '__main__':
-    app.run(port=5000, debug=False)
-    
-    
-    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
     
     
